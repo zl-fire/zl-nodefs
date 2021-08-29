ae47ba64b8c948a894b3719b4537696741482e45
zhangluzhanglu <1758658975@qq.com>
2021-08-29 17:42:48 +0800

    [feat]读取文件内容时,动态添加编码参数

--- src/main.js
+++ src/main.js
@@ -21,11 +21,9 @@ module.exports = {
 // var fileList = readFileList({
 //     dirPath: "src",  //读取src下的所有文件
 //     ignoreList: ["node_modules", ".git"], //遇到node_modules目录时进行忽略
-//     // needTypes: [".doc", ".docx"], //只读取".doc", ".docx" 类型文件
-//     // ignoreTypes:[".js",".doc"], //忽略".js",".doc"文件类型(如果needTypes存在，则以needTypes为准，会忽略ignoreTypes参数)
 // })
 // console.log(JSON.stringify(fileList, null, 4));
 
 // // 读取文件内容
-// let res = readFileContent({ filePath: "./src/main.js", returnType: "string" });
+// let res = readFileContent({ filePath: "./src/main.js" });
 // console.log("=====res====", res);
--- src/module/readFileContent.js
+++ src/module/readFileContent.js
@@ -4,18 +4,19 @@ var fs = require('fs'); // 引入fs模块
     * @function 同步方式，读取指定目录下的所有文件/文件夹列表,返回tree结构数据
     * @param {Object} paramsObj 完整的参数对象信息
     * @param {String} paramsObj.filePath 要读取的文件路径，可传入相对路径(./,../),也可传入绝对路径
+    * @param {String} paramsObj.readEncode 表示以何种编码读取文件，默认值为'utf-8'
     * @param {buffer|string} paramsObj.returnType 将读取结果以指定类型返回，默认返回字符串类型，可选值：buffer,string
     * @return {buffer|string} 返回buffer|string类型数据
     * @author zl-fire 2021/08/29
     * @example
-    * let res = readFileContent({ filePath: "./src/main.js", returnType: "string" });
+    * let res = readFileContent({ filePath: "./src/main.js" });
     * console.log("=====res====", res);
   */
 function readFileContent(paramsObj) {
-    let { filePath, returnType = "string" } = paramsObj;
-    const buffer = fs.readFileSync(filePath);
+    let { filePath, readEncode = 'utf-8', returnType = "string" } = paramsObj;
+    const buffer = fs.readFileSync(filePath, readEncode);
     if (returnType == "string") return String(buffer);
     else return buffer;
 }
 
-module.exports=readFileContent;
+module.exports = readFileContent;
